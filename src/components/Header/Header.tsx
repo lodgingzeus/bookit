32c0162afb0ff34468fd7b5f3a950266b613b2d7
lodgingzeus <bhagatd585@gmail.com>
2023-06-02 11:10:21 +0530

    sign in page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
       <>
           <header className="flex justify-between border p-4 bg-[#fdfbfb]">
               <div>logo</div>
-              <Link href="/auth/signup" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Login</Link>
+              <Link href="/auth/signup" className="bg-transparent hover:bg-[#D03F71] text-[#317C63] font-semibold hover:text-white py-2 px-4 border border-[#317C63] hover:border-transparent rounded">Login</Link>
           </header>
       </>
     )
