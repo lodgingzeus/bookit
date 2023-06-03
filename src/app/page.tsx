16af7ba7ab90f5761db8a9def29d7307deb61838
tanmaydot <reachtanmays@gmail.com>
2023-06-03 20:23:38 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Header from "@/components/Header/Header"
+/* eslint-disable react/no-unescaped-entities */
 
-export default function Home() {
+import Header from "@/components/Header/page";
+import Image from "next/image";
+
+export default function ROOT() {
   return (
-    <div className="">
+    <div className="header">
       <Header />
+        <div className="flex">
+          <div className="right "> 
+          <section>
+              <div>
+                <p>
+                  Make easy hotel reservation's to The Worlds best HOTELS.
+                </p>
+              </div>
+              <div className="button">
+                <button>BookIT</button>
+              </div>
+          </section>
+          </div>
+          <div>
+            <div className="w-1/2 right-full left-0">
+              <Image src="/img.jpg" width={100} height={100} alt="Hotel"/>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
