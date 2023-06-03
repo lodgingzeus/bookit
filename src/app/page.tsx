dbef1241babe679a6989266c2f1fb678b8ce179c
tanmaydot <reachtanmays@gmail.com>
2023-06-04 01:05:15 +0530

    homepage commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,41 @@
 /* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import Header from '@/components/Header/page';
+import Footer from '@/components/Footer/page';
+import Image from 'next/image';
+import img from 'src/app/images/img.jpg';
 
-import Header from "@/components/Header/page";
-import Image from "next/image";
-
-export default function ROOT() {
+export default function Home() {
   return (
-    <div className="header">
-      <Header />
-        <div className="flex">
-          <div className="right "> 
-          <section>
-              <div>
-                <p>
-                  Make easy hotel reservation's to The Worlds best HOTELS.
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-gray-200">
+        <Header />
+      </header>
+      <main className="flex-grow">
+        <div className="flex justify-center">
+          <div className="w-full relative">
+            <div className="h-[calc(100vh-64px)]">
+              <Image src={img} alt="Hotel" className="object-cover h-full w-full filter brightness-55 hover:brightness-100 transition duration-500 ease-in-out" />
+            </div>
+            <div className="absolute flex flex-col items-start justify-center w-full h-full top-0 left-0 bg-black bg-opacity-50">
+              <div className="p-8">
+                <p className="text-white text-lg font-semibold mb-4">
+                  Experience unparalleled luxury and reserve your stay at the world's most prestigious hotels.
                 </p>
+                <p className="text-white text-sm font-light mb-8">
+                  Unlock extraordinary moments with our hassle-free hotel reservations, where sophistication meets perfection.
+                </p>
+                <button className="bg-white text-black hover:bg-black hover:text-white border border-black hover:border-transparent rounded-full py-2 px-4 transition duration-500 ease-in-out">
+                  BookIT
+                </button>
               </div>
-              <div className="button">
-                <button>BookIT</button>
-              </div>
-          </section>
-          </div>
-          <div>
-            <div className="w-1/2 right-full left-0">
-              <Image src="/img.jpg" width={100} height={100} alt="Hotel"/>
             </div>
           </div>
-      </div>
+        </div>
+      </main>
+      <footer className="bg-gray-200">
+        <Footer />
+      </footer>
     </div>
-  )
+  );
 }
--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import Link from "next/link"
 
 const Header = () => {
     return (
       <>
           <header className="flex justify-between border p-4 bg-[#fdfbfb]">
-              <div>logo</div>
+              <div>HEADER</div>
               <Link href="/auth/signup" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Login</Link>
           </header>
       </>
