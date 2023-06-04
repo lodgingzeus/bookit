4593962e3863b936ed4ab4e248580443043cc53a
tanmaydot <reachtanmays@gmail.com>
2023-06-04 14:51:09 +0530

    cleanandbetter

---
+++ src/components/Hotels/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import img1 from 'src/app/images/img1.jpg';
+import img2 from 'src/app/images/img2.jpg';
+import img3 from 'src/app/images/img3.jpg';
+import img4 from 'src/app/images/img4.jpg';
+
+import React from 'react'
+
+export const Hotels = () => {
+  return (
+    <div>Hotels</div>
+  )
+}
+export default Hotels;
