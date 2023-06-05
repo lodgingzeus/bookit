db7014e768defd120e5236d2e96051026d9ec323
tanmaydot <reachtanmays@gmail.com>
2023-06-05 13:54:50 +0530

    filing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import Home from './home/page';
+import Home from '../components/home/page';
 import Hotels from '@/components/Hotels/page';
 import Header from '@/components/Header/page';
 import Footer from '@/components/Footer/page';
